00bff6bf3e1a3f2d2aacf8400d3438aac66087b6
lixuan310 <lixuan19900214@gmail.com>
2017-05-29 22:54:11 +0800

    navbar and footer

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,13 @@ Rails.application.routes.draw do
      resources :orders
    end
 
+   resources :statics do
+     collection do
+       get :authorintro
+       get :roleintro
+     end
+   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
